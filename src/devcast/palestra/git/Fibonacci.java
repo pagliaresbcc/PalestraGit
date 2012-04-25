b9644709793c4218ad49fb70549798604701f2a0
Rodrigo Pagliares <pagliares@gmail.com>
2012-04-24 23:58:23 -0300

    Primeiro commit. Teste de Fibonacci de 0 e sua implementacao

---
+++ src/devcast/palestra/git/Fibonacci.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package devcast.palestra.git;
+
+/**
+ *
+ * @author pagliares
+ */
+public class Fibonacci {
+    public int calcularFibonacci(int numero){
+        return 0;
+    }
+    
+}
---
+++ test/devcast/palestra/git/TesteFibonacci.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package devcast.palestra.git;
+
+import org.junit.AfterClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.BeforeClass;
+
+/**
+ *
+ * @author pagliares
+ */
+public class TesteFibonacci {
+    
+    @Test
+    public void testFibonacci() {
+        Fibonacci fibonacci = new Fibonacci();
+        assertEquals(0, fibonacci.calcularFibonacci(0));
+    }
+    
+     
+}
